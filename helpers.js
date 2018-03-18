adfae43c8c64116e900df4ec71f2f62e95a9679b
kegriz <rafalrudko.cmzzs@gmail.com>
2018-03-18 12:12:22 +0100

    JS files imported

---
+++ helpers.js
@@ -0,0 +1,20 @@
+export default class Helpers {
+
+  constructor(args) {
+    // code
+  }
+
+  // static sprawia ze w index.js nie trzeba tworzyc instancji tej klasy
+  // działa od razu Helpers.test()
+  static test() {
+    return 'Meow'
+  }
+
+  static hello() {
+    return 'hello Meow'
+  }
+
+  goodbye() {
+    return 'bye'
+  }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import Helpers from './helpers.js';
+
+const helpers = new Helpers;
+
+const test = helpers.goodbye()
+
+console.log(test)
+console.log(Helpers.test())
